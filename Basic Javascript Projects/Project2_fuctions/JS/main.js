7f22bc0b54ed63add9a2f185271b9c0ce022bb68
Victoria Ransom <victoriamransom@gmail.com>
2023-08-10 14:17:03 -0700

    Project2_functions submissions assignment

---
+++ Basic Javascript Projects/Project2_fuctions/JS/main.js
@@ -0,0 +1,5 @@
+function myFunction() {
+    var sentence = "I am learning"; //Assigning a variable
+    sentence+= " a lot from this class!"; //concatenating the string
+    document.getElementById("Concatenate").innerHTML=sentence; //displaying the concatenated string
+}
