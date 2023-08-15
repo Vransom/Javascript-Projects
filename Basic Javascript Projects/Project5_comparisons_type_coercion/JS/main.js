00fee1bd2cba27c3dfb832f4731aeaa71f566e7e
Victoria Ransom <victoriamransom@gmail.com>
2023-08-15 12:50:38 -0700

    project5_comparisons_type_coercion

---
+++ Basic Javascript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,30 @@
+function not_function() {
+    document.getElementById("not").innerHTML=!(20>10);
+}
+
+document.write("10"+5); //coercion, combining a string and an umber
+
+document.write("<br>"); //line break
+
+document.write(10==10);
+
+document.write("<br>");
+A="13"
+B="13"
+document.write(A===B);
+
+document.write("<br>");
+
+document.write(15>10);
+
+document.write("<br>");
+
+document.write(10<15);
+
+document.write("<br>");
+
+document.write(10>15 && 15>10);
+
+document.write("<br>");
+
+document.write(5>10 || 10>4);
