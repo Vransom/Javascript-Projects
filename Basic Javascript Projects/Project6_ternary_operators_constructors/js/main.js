7b0fc7be005728c839e85df802e92c4b293a9a90
Victoria Ransom <victoriamransom@gmail.com>
2023-08-16 09:48:21 -0700

    update

--- Basic Javascript Projects/Project6_ternary_operators_constructors/js/main.js
+++ Basic Javascript Projects/Project6_ternary_operators_constructors/js/main.js
@@ -1,11 +1,11 @@
-function Vote_Function() {
+function Vote_Function() { //Ternary
     var Age, Can_vote;
     Age=document.getElementById("Age").value;
     Can_vote=(Age<18)?"You are not old enough":"You are old enough";
     document.getElementById("Vote").innerHTML=Can_vote+" to vote.";
 }
 
-function Vehicle(Make, Model, Year, Color) {
+function Vehicle(Make, Model, Year, Color) { //new and this
     this.Vehicle_Make=Make;
     this.Vehicle_Model=Model;
     this.Vehicle_Year=Year;
@@ -20,7 +20,7 @@ function myFunction() {
     + Erik.Vehicle_Color+ "-colored " +Erik.Vehicle_Model+ " manufactured in "+Erik.Vehicle_Year;
 }
 
-function count_function() {
+function count_function() { //nested function
     document.getElementById("Nestedfunction").innerHTML=count();
     function count() {
         var starting_point=9;
